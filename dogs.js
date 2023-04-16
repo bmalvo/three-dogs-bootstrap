03ea11168de5e04f73f94c09c74019b67aa08dce
patryk_s <boydmalvo@gmail.com>
2023-04-16 17:08:36 +0200

    add image slider to gallery

--- dogs.js
+++ dogs.js
@@ -1,5 +1,27 @@
-var fullImgBox = document.getElementById("fullImgBox")
-var fullImg = document.getElementById("fullImg")
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+let imagesDogs = ["./gallery/bis-and-choco (1).jpg", "./gallery/gallery_1_fix.jpg", "./gallery/gallery_2_fix.jpg" , "./gallery/gallery_3_fix.jpg", "./gallery/gallery_4 _fix.jpg", "./gallery/gallery_5_fix.jpg", "./gallery/gallery_6_fix.jpg", "./gallery/gallery_7_fix.jpg", "./gallery/gallery_8_fix.jpg", "./gallery/gallery_9_fix.jpg", "./gallery/gallery_10_fix.jpg", "./gallery/gallery_11_fix.jpg", "./gallery/gallery_12_fix.jpg"]
+
+let num = 0;
+
+function next(){
+    let fullImg = document.getElementById("fullImg");
+    num++;
+    if (num >= imagesDogs.length) {
+        num = 0;
+    }
+    fullImg.src = imagesDogs[num];
+}
+
+function prev(){
+    let fullImg = document.getElementById("fullImg");
+    num--;
+    if (num < 0) {
+        num = imagesDogs.length -1;
+    }
+    fullImg.src = imagesDogs[num];
+}
 
 function openFullImg (pic) {
     fullImgBox.style.display = "flex";
