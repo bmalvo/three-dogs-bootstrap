47c95d9900ede63c405ea2bdbea5bb946362a82f
patryk_s <boydmalvo@gmail.com>
2023-04-17 20:27:15 +0200

    add shortcuts from keyboard to image slider

--- dogs.js
+++ dogs.js
@@ -1,7 +1,13 @@
 var fullImgBox = document.getElementById("fullImgBox");
 var fullImg = document.getElementById("fullImg");
 
-let imagesDogs = ["./gallery/bis-and-choco (1).jpg", "./gallery/gallery_1_fix.jpg", "./gallery/gallery_2_fix.jpg" , "./gallery/gallery_3_fix.jpg", "./gallery/gallery_4 _fix.jpg", "./gallery/gallery_5_fix.jpg", "./gallery/gallery_6_fix.jpg", "./gallery/gallery_7_fix.jpg", "./gallery/gallery_8_fix.jpg", "./gallery/gallery_9_fix.jpg", "./gallery/gallery_10_fix.jpg", "./gallery/gallery_11_fix.jpg", "./gallery/gallery_12_fix.jpg"]
+let imagesDogs = ["./gallery/bis-and-choco (1).jpg",
+ "./gallery/gallery_1_fix.jpg", "./gallery/gallery_2_fix.jpg" , 
+ "./gallery/gallery_3_fix.jpg", "./gallery/gallery_4 _fix.jpg", 
+ "./gallery/gallery_5_fix.jpg", "./gallery/gallery_6_fix.jpg", 
+ "./gallery/gallery_7_fix.jpg", "./gallery/gallery_8_fix.jpg", 
+ "./gallery/gallery_9_fix.jpg", "./gallery/gallery_10_fix.jpg", 
+ "./gallery/gallery_11_fix.jpg", "./gallery/gallery_12_fix.jpg"]
 
 let num = 0;
 
@@ -31,3 +37,32 @@ function openFullImg (pic) {
 function closeFullImg() {
     fullImgBox.style.display = "none";
 }
+
+let prevButton=document.getElementById("prev");
+document.body.addEventListener("keydown",function(){
+    let code=37; // 37 is keycode of 'left-arrow'
+    let code2 = 65 // 65 is keycode of 'A'
+    if(event.keyCode==code || event.keyCode == code2)
+    {
+        prevButton.click();
+    }
+});
+
+let nextButton=document.getElementById("next");
+document.body.addEventListener("keydown",function(){
+    let code = 39; // 39 is keycode of 'right-arrow'
+    let code2 = 68 // 68 is keycode of 'D'
+    if(event.keyCode==code || event.keyCode == code2)
+    {
+        nextButton.click();
+    }
+});
+
+let closeButton=document.getElementById("close");
+document.body.addEventListener("keydown",function(){
+    let code=27; // 27 is keycode of 'escape'
+    if(event.keyCode==code)
+    {
+        closeButton.click();
+    }
+});
