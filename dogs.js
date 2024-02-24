a4ab936bb76ad285ed2cbe12c24e71e784dcdf65
bmalvo <boydmalvo@gmail.com>
2024-02-24 12:40:51 +0100

    update event keydown

--- dogs.js
+++ dogs.js
@@ -31,38 +31,39 @@ function prev(){
 
 function openFullImg (pic) {
     fullImgBox.style.display = "flex";
-    fullImg.src = pic;
+  fullImg.src = pic;
+  
 }
 
 function closeFullImg() {
     fullImgBox.style.display = "none";
 }
 
-let prevButton=document.getElementById("prev");
-document.body.addEventListener("keydown",function(){
-    let code=37; // 37 is keycode of 'left-arrow'
-    let code2 = 65 // 65 is keycode of 'A'
-    if(event.keyCode==code || event.keyCode == code2)
-    {
-        prevButton.click();
+const prevButton = document.getElementById("prev");
+
+document.body.addEventListener("keydown", (e) => {
+    const key1 = 'ArrowLeft'; 
+    const key2 = 'a';
+    if(e.key === key1 || e.key === key2) {
+      prevButton.click();
     }
 });
 
-let nextButton=document.getElementById("next");
-document.body.addEventListener("keydown",function(){
-    let code = 39; // 39 is keycode of 'right-arrow'
-    let code2 = 68 // 68 is keycode of 'D'
-    if(event.keyCode==code || event.keyCode == code2)
-    {
-        nextButton.click();
+const nextButton = document.getElementById("next");
+
+document.body.addEventListener("keydown", (e) => {
+    const key1 = 'ArrowRight';
+    const key2 = 'd';
+    if(e.key === key1 || e.key === key2) {
+      nextButton.click();
     }
 });
 
-let closeButton=document.getElementById("close");
-document.body.addEventListener("keydown",function(){
-    let code=27; // 27 is keycode of 'escape'
-    if(event.keyCode==code)
-    {
-        closeButton.click();
+const closeButton = document.getElementById("close");
+
+document.body.addEventListener("keydown", (e) => {
+    const key = 'Escape';
+    if(e.key === key) {
+      closeButton.click();
     }
 });
